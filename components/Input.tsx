77526cbc04d28a51f4b9624fbe9e8b87373bbd83
herohasan0 <kahraman.h@outlook.com>
2023-09-25 01:22:05 +0300

    update input ui

--- components/Input.tsx
+++ components/Input.tsx
@@ -12,7 +12,7 @@ export default function Input({ value, onChange, placeholder }: InputType) {
       value={value}
       type="text"
       onChange={onChange}
-      className="rounded h-[40px] pl-4 text-base outline-0 focus:outline-1 focus:outline-heroic-blue disabled:bg-insiderTheme-Greys/border border border-insiderTheme-Greys/border bg-[#F7F9FA] placeholder:text-[15px]"
+      className="rounded-lg h-[36px] pl-4 text-base outline-0 focus:outline-1 focus:outline-blue-500 border border-gray-200 bg-custom-gray-50 placeholder:text-[15px]"
       placeholder={placeholder}
     />
   );
