99af97536ccd17f29b5444c11b9e9c992edace23
herohasan0 <kahraman.h@outlook.com>
2023-09-24 12:29:37 +0300

    update query client default options

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,17 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * (60 * 1000), //10 min
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
