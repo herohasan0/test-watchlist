792ea3344b1d2438db881b69a4a136b06ef08361
herohasan0 <kahraman.h@outlook.com>
2023-09-24 12:23:32 +0300

    create a json file and send it to your app

--- pages/api/companies.ts
+++ pages/api/companies.ts
@@ -1,5 +1,12 @@
 import { services } from "@/services/api";
 import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+
+const createJsonFileName = (): string => {
+  const ms = Date.now();
+  return `${ms}.json`;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,26 +15,79 @@ export default async function handler(
   const symbols = ["AAPL", "MSFT", "IBM"];
   const companyData: any = {};
 
-  const axiosPromises = symbols.map((symbol) => services.overview({ symbol }));
+  const rootFolder = process.cwd();
+  const folderPath = path.join(rootFolder, "datafile");
+  const fileName = createJsonFileName();
+  const filePath = path.join(folderPath, fileName);
 
-  await Promise.all(axiosPromises)
-    .then((responses) => {
-      symbols.forEach((symbol, index) => {
-        companyData[symbol] = {
-          name: responses[index].data.Name || "name placeholder",
-          description: responses[index].data.Description || "desc placeholder",
-          address: responses[index].data.Address || "address placeholder",
-          dividendYield:
-            responses[index].data.DividendYield || "dividedyield placeholder",
-          marketCapitalization:
-            responses[index].data.MarketCapitalization ||
-            "marketcapital placeholder",
-        };
-      });
-    })
-    .catch((error) => {
-      console.error("An error occurred:", error);
+  function removeFolderRecursive(folderPath: string) {
+    fs.readdirSync(folderPath).forEach((file) => {
+      const curPath = path.join(folderPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        removeFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
     });
+    fs.rmdirSync(folderPath);
+  }
 
-  res.status(200).json(companyData);
+  if (!fs.existsSync(folderPath)) {
+    const axiosPromises = symbols.map((symbol) =>
+      services.overview({ symbol })
+    );
+
+    console.log("service call here");
+    await Promise.all(axiosPromises)
+      .then((responses) => {
+        symbols.forEach((symbol, index) => {
+          companyData[symbol] = {
+            name: responses[index].data.Name || "name placeholder",
+            description:
+              responses[index].data.Description || "desc placeholder",
+            address: responses[index].data.Address || "address placeholder",
+            dividendYield:
+              responses[index].data.DividendYield || "dividedyield placeholder",
+            marketCapitalization:
+              responses[index].data.MarketCapitalization ||
+              "marketcapital placeholder",
+          };
+        });
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+
+    Object.assign(companyData, { fileName });
+
+    const jsonData = JSON.stringify(companyData, null, 2);
+
+    try {
+      if (fs.existsSync(folderPath)) {
+        removeFolderRecursive(folderPath);
+      }
+
+      fs.mkdirSync(folderPath, { recursive: true });
+      fs.writeFileSync(filePath, jsonData);
+    } catch (error) {
+      console.error("Error creating JSON file:", error);
+      res.status(500).json({ error: `Error creating JSON file: ${error}` });
+    }
+  }
+
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error("Error reading folder:", err);
+      res.status(500).end("Internal Server Error");
+      return;
+    }
+    if (files.length === 0) {
+      res.status(404).end("No files found in the folder");
+      return;
+    }
+    const chosenFile = files[0];
+    const filePath = path.join(folderPath, chosenFile);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    return res.status(200).send(fileContent);
+  });
 }
