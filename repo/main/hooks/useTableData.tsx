d8fee176e37896270f1bfb55725c09cc8ad4f883
herohasan0 <kahraman.h@outlook.com>
2023-10-01 16:11:04 +0300

    update get table data query

--- repo/main/hooks/useTableData.tsx
+++ repo/main/hooks/useTableData.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "react-query";
 import { getTableData } from "@/repo/main";
 
+const symbols = ["AAPL", "IBM"];
+
 export const useTableData = () => {
-  return useQuery(getTableData());
+  return useQuery(getTableData(symbols));
 };
